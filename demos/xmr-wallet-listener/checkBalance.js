bff4dff0819ee0a193efeb40f514bf25c0a2cab7
b4n6-b4n6 <b4n6-b4n6@yogi.pro>
2026-06-27 07:52:54 +0700

    rev

---
+++ demos/xmr-wallet-listener/checkBalance.js
@@ -0,0 +1,8 @@
+/*
+  start from subaddress 0
+  test whether each subaddress has balance
+  discard those with balance and add those without balance into a pool
+
+  curl example for get subaddress
+  curl example for get balance
+*/
