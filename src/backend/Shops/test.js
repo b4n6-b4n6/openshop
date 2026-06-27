e7ff4aa91eb434834426105fdaf8157aec76f154
b4n6-b4n6 <b4n6-b4n6@yogi.pro>
2026-06-27 14:02:24 +0700

    rev

---
+++ src/backend/Shops/test.js
@@ -0,0 +1,3 @@
+test('does 1 == 1', () => {
+  expect(1).toEqual(1);
+});
--- src/frontend/middlewares/backend.js
+++ src/frontend/middlewares/backend.js
@@ -1,14 +1,17 @@
+// import createShops from '../../backend/Shops.js';
 // import createProducts from '../../backend/Products.js';
 // import createOrders from '../../backend/Orders.js';
 import createPool from '../../backend/createPool.js';
 
 export default async () => {
   const pool = createPool();
+  // const shops = await createShops(pool);
   // const products = await createProducts(pool);
   // const orders = await createOrders(pool);
 
   return async (ctx, next) => {
     ctx.backend = {
+      // shops,
       // products,
       // orders,
     };
