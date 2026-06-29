08c4b9c8d30f94169fba35b3774a95550a1d4e7d
b4n6-b4n6 <b4n6-b4n6@yogi.pro>
2026-06-29 07:58:18 +0700

    Improve Shops tests

--- src/backend/Shops/test.js
+++ src/backend/Shops/test.js
@@ -1,9 +1,11 @@
 import createShops from './index.js';
 
+const ONION_ADDRESS = 'http://2gzyxa5ihm7nsggfxnu52rck2vv4rvmdlkiu3zzui5du4xyclen53wid.onion'
+
 test('can get no shop', async () => {
   const shops = await createShops();
 
-  expect(await shops.get('example.onion')).toEqual(undefined);
+  expect(await shops.get(ONION_ADDRESS)).toEqual(undefined);
 
   await shops.destroy();
 });
@@ -12,14 +14,14 @@ test('can create a shop', async () => {
   const shops = await createShops();
 
   await shops.update({
-    address: 'example.onion',
+    address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
     name: 'Test Shop',
     description: 'We offer to test stuffs!',
   });
-  expect(await shops.get('example.onion')).toEqual({
-    address: 'example.onion',
+  expect(await shops.get(ONION_ADDRESS)).toEqual({
+    address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
     name: 'Test Shop',
@@ -33,21 +35,21 @@ test('can update a shop', async () => {
   const shops = await createShops();
 
   await shops.update({
-    address: 'example.onion',
+    address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
     name: 'Test Shop',
     description: 'We offer to test stuffs!',
   });
   await shops.update({
-    address: 'example.onion',
+    address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
     name: 'Test Shop',
     description: 'We offer to test many stuffs!',
   });
-  expect(await shops.get('example.onion')).toEqual({
-    address: 'example.onion',
+  expect(await shops.get(ONION_ADDRESS)).toEqual({
+    address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
     name: 'Test Shop',
