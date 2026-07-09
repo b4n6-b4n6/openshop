7ede64f798d8b5026c77cb916ada16083b6b2fc7
b4n6-b4n6 <b4n6-b4n6@yogi.pro>
2026-07-09 07:23:38 +0700

    Add test for Shops

--- src/backend/Shops/test.js
+++ src/backend/Shops/test.js
@@ -31,7 +31,7 @@ test('can create a shop', async () => {
   await shops.destroy();
 });
 
-test('can update a shop', async () => {
+test('can update a shop name, description', async () => {
   const shops = await createShops();
 
   await shops.update({
@@ -45,13 +45,69 @@ test('can update a shop', async () => {
     address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
-    name: 'Test Shop',
+    name: 'Test Shopz',
     description: 'We offer to test many stuffs!',
   });
   expect(await shops.get(ONION_ADDRESS)).toEqual({
     address: ONION_ADDRESS,
     profile_photo: null,
     banner_photo: null,
+    name: 'Test Shopz',
+    description: 'We offer to test many stuffs!',
+  });
+
+  await shops.destroy();
+});
+
+test('can update a shop profile_photo, banner_photo (a)', async () => {
+  const shops = await createShops();
+
+  await shops.update({
+    address: ONION_ADDRESS,
+    profile_photo: Buffer.from("deadbeef", "hex"),
+    banner_photo: Buffer.from("d222d222", "hex"),
+    name: 'Test Shop',
+    description: 'We offer to test many stuffs!',
+  });
+  expect(await shops.get(ONION_ADDRESS)).toEqual({
+    address: ONION_ADDRESS,
+    profile_photo: Buffer.from("deadbeef", "hex"),
+    banner_photo: Buffer.from("d222d222", "hex"),
+    name: 'Test Shop',
+    description: 'We offer to test many stuffs!',
+  });
+
+  await shops.destroy();
+});
+
+test('can update a shop name, description, profile_photo, banner_photo (b)', async () => {
+  const shops = await createShops();
+
+  await shops.update({
+    address: ONION_ADDRESS,
+    profile_photo: null,
+    banner_photo: null,
+    name: 'Test Shop',
+    description: 'We offer to test stuffs!',
+  });
+  await shops.update({
+    address: ONION_ADDRESS,
+    profile_photo: Buffer.from("deadbeef", "hex"),
+    banner_photo: Buffer.from("d222d222", "hex"),
+    name: 'Test Shop',
+    description: 'We offer to test many stuffs!',
+  });
+  await shops.update({
+    address: ONION_ADDRESS,
+    profile_photo: null,
+    banner_photo: null,
+    name: 'Test Shop',
+    description: 'We offer to test many stuffs!',
+  });
+  expect(await shops.get(ONION_ADDRESS)).toEqual({
+    address: ONION_ADDRESS,
+    profile_photo: Buffer.from("deadbeef", "hex"),
+    banner_photo: Buffer.from("d222d222", "hex"),
     name: 'Test Shop',
     description: 'We offer to test many stuffs!',
   });
