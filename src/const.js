21564bc00091b5ed9ce94e57dd01d9ecc19f5aad
b4n6-b4n6 <b4n6-b4n6@yogi.pro>
2026-07-10 13:56:25 +0700

    Implement fetchFiatPrice

--- src/const.js
+++ src/const.js
@@ -4,3 +4,6 @@ export const JWT_SECRET = (
   Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex')
 );
 export const USER_CLAIM_TYPE = 'USER_CLAIM';
+export const CURRENCIES = [
+  'btc', 'eur', 'usd', 'aud', 'nzd', 'gbp', 'cny', 'jpy', 'rub',
+];
---
+++ src/onion_frontend/utils/fetchFiatPrice.js
@@ -0,0 +1,30 @@
+import { CoinGeckoClient } from 'coingecko-api-v3';
+import createDebounced from './createDebounced.js';
+import { CURRENCIES } from '../../const.js';
+
+const client = new CoinGeckoClient({
+  timeout: 30000,
+});
+const fetchFiatPrice = async () => {
+  const response = await client.simplePrice({
+    ids: 'monero',
+    vs_currencies: CURRENCIES,
+  });
+
+  if (!response.success) {
+    console.error(response.data);
+    return null;
+  }
+
+  const result = response.data?.monero;
+
+  if (!result) {
+    console.error(result);
+    return null;
+  }
+
+  return result;
+};
+
+const REMEMBER_FOR = 1000 * 60 * 90;
+export default createDebounced(fetchFiatPrice, REMEMBER_FOR);
